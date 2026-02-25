9779ef5b7ae46acb5cf8c4467473724ceca1e747
SP <23213394+palcsta@users.noreply.github.com>
2026-02-25 14:50:53 +0200

    style: update footer year range to 2021-2026

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     return (
       <div style={center}>
         <b><i><a href="https://fullstackopen.com">FullStackOpen</a> 
-        <a href="https://courses.helsinki.fi/fi/aytkt21010/133171831"> project</a> 2021, University of Helsinki</i></b>
+        <a href="https://courses.helsinki.fi/fi/aytkt21010/133171831"> project</a> 2021-2026, University of Helsinki</i></b>
       </div>
     )
   }
